764f69771976cf94652e2d56a76c3fac03566d0f
Emilyempa <emilypettersson@hotmail.com>
2025-01-16 19:08:09 +0100

    Fixed css

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,14 @@ const app = express();
 const port = 5080;
 
 app.use(bodyParser.json());
-// app.use('/assets', express.static(path.join(__dirname, '../dist/assets/main-4swr1hn8.css')));
-// app.use('/assets', express.static(path.join(__dirname, '../dist/asset/main-4swr1hn8.css')));
-app.use(express.static(path.join(__dirname, '../dist/assets/main-4swr1hn8.css')));
+
+app.use('/assets', (req, res, next) => {
+  if (req.path.endsWith('.css')) {
+    express.static(path.join(__dirname, '../dist/assets'))(req, res, next);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, '../dist/index.html');
@@ -27,9 +32,41 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.use(express.static(path.join(__dirname, '../dist/assets/main-4swr1hn8.css')));
+app.get('/kids', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/kids.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred, Try again soon');
+      return;
+    }
+    res.send(data);
+  });
+});
 
-// app.use("/static", express.static('../dist/assets/main-4swr1hn8.css'));
+app.get('/about', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/about.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred, Try again soon');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.get('/:id', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/about.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred, Try again soon');
+      return;
+    }
+    res.send(data);
+  });
+});
 
 app.listen(5080, () => {
   console.log(`Server is running on port ${port}`);
