64ebba0a41f1bf5e60b2bb810dfdc742618308ca
Emilyempa <emilypettersson@hotmail.com>
2025-01-21 12:24:51 +0100

    Add jest, supertest and cross-env. Also add test folder and test file and tested that npm test works fine and passes

---
+++ backend/test/id.test.js
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals';
+import request from 'supertest';
+
+// import app from './index.js';
+
+test('/:id page shows right title', async () => {
+  // Something
+});
