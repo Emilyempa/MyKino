69166e52831293b4467e3e0daa52ffc2661ab093
Emilyempa <emilypettersson@hotmail.com>
2025-01-22 16:34:01 +0100

    Add logic for getting data from json to header and footer

--- backend/index.js
+++ backend/index.js
@@ -27,41 +27,70 @@ const getMovies = async () => {
   }
 };
 
-const getFooter = async () => {
-  try {
-    const filePath = path.join(__dirname, '../dist/data/footer.json');
-    const data = fs.readFileSync(filePath, 'utf8'); //change to await fs.readFile
-    const jsonData = JSON.parse(data);
-    return console.log(JSON.stringify(jsonData, null, 2));
-    // return jsonData; to send to handlebars
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    // return null;
-  }
-};
+// const getFooter = async () => {
+//   try {
+//     const filePath = path.join(__dirname, '../dist/data/footer.json');
+//     const data = fs.readFileSync(filePath, 'utf8'); //change to await fs.readFile
+//     const jsonData = JSON.parse(data);
+//     return console.log(JSON.stringify(jsonData, null, 2));
+//     // return jsonData; to send to handlebars
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     // return null;
+//   }
+// };
 
-getFooter();
+// getFooter();
+const footer = JSON.parse(fs.readFileSync(path.join(__dirname, '../dist/data', 'footer.json'), 'utf8'));
+const header = JSON.parse(fs.readFileSync(path.join(__dirname, '../dist/data', 'header.json'), 'utf8'));
 
 app.use('/assets', express.static(path.join(__dirname, '../dist/assets')));
 app.use('/data', express.static(path.join(__dirname, '../dist/data')));
 app.use('/img', express.static(path.join(__dirname, '../dist/img')));
 
-app.get('/', async (req, res) => {
+// app.use((req, res, next) => {
+//   res.locals.footer = getfooter();
+//   res.locals.header = header;
+//   next();
+// });
+app.use((req, res, next) => {
+  const footerSpecifics = {
+    logo: footer.footer.logo,
+    text: footer.footer.text,
+    sections: footer.footer.sections,
+  };
+  res.locals.footer = footerSpecifics;
+  next();
+});
+
+app.use((req, res, next) => {
+  const headerSpecifics = {
+    logo: header.header.mainHeader.logo,
+    brandName: header.header.mainHeader.brandName,
+    alt: header.header.mainHeader.alt,
+    menuLogo: header.header.hamburgerMenu.menuLogo,
+    menuLinks: header.header.hamburgerMenu.menuLinks,
+  };
+  res.locals.header = headerSpecifics;
+  next();
+});
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/index.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('An error occurred, Try again soon');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.get('/movies', async (req, res) => {
   const movies = await getMovies();
   res.render('home', { movies });
 });
-
-// app.get('/', (req, res) => {
-//   const filePath = path.join(__dirname, '../dist/index.html');
-//   fs.readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(404).send('An error occurred, Try again soon');
-//       return;
-//     }
-//     res.send(data);
-//   });
-// });
 
 app.get('/kids', (req, res) => {
   const filePath = path.join(__dirname, '../dist/kids.html');
