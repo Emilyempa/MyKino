cb9227512d5067b75cc73fa4320d44cf274844a7
Emilyempa <emilypettersson@hotmail.com>
2025-01-23 12:59:38 +0100

    remove unesessery code import

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import exp from 'constants';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
